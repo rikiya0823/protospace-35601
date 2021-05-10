edf1b987a457d0f64cac4a021a1a74ef97d1631d
rikiya0823 <3skkt8xrb@gmail.com>
2021-05-10 23:45:30 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,64 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :update, :show]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    if @prototype.destroy
+       redirect_to root_path
+     else
+       render :new
+     end    
+   end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:image, :title, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    if user_signed_in?
+      redirect_to action: :index
+    end
+  end
+end
